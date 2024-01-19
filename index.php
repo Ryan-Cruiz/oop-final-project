68999d38586548ce8787441cc133a9d7ca3cc1b1
clyde caneso <clydevergara28@gmail.com>
2024-01-19 19:11:33 +0800

    landing page

--- index.php
+++ index.php
@@ -49,14 +49,14 @@
     <header id="header" class="d-flex align-items-center">
         <div class="container d-flex align-items-center justify-content-between">
 
-            <h1 class="logo"><a href="index.html">GLC<span>.</span></a></h1>
+            <h1 class="logo" style><a href="index.html"><img src="assets/img/glc.png" > Golden Link College<span></span></a></h1>
             <!-- Uncomment below if you prefer to use an image logo -->
             <!-- <a href="index.html" class="logo"><img src="assets/img/logo.png" alt=""></a>-->
 
             <nav id="navbar" class="navbar">
                 <ul>
                     <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
-                    <li><a class="nav-link scrollto" href="#blog">Blogs</a></li>
+                    <li><a class="nav-link scrollto" href="#blog">About</a></li>
                     <li><a href="backend/login.php">Login</a></li>
                 </ul>
                 <i class="bi bi-list mobile-nav-toggle"></i>
@@ -69,7 +69,7 @@
     <section id="hero" class="d-flex align-items-center">
         <div class="container" data-aos="zoom-out" data-aos-delay="100">
             <h1>Welcome to <span>Golden Link College</span></h1>
-            <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit, praesentium.</h2>
+            <h2>BE THE BEST THAT YOU CAN BE.</h2>
             <div class="d-flex">
                 <a href="#blog" class="btn-get-started scrollto">Get Started</a>
             </div>
@@ -79,8 +79,85 @@
         <!-- ======= About Section ======= -->
         <section id="blog" class="about section-bg">
             <div class="container" data-aos="fade-up">
+                <div class="courses" id="aboutus" >
+                    <h1>The Golden Link College</h1>
+                    <h2>adopts an educational approach that</h2>
+                    <div class="column1 "  >
+                        <p>• Gives importance to the development of life skills and character building, such as self-awareness, integration of values, effective relationships, conflict resolution, public speaking, etc., side by side with academic competencies;
+<br>
 
-                <div class="section-title">
+• Motivates students to study by awakening their interest in the subject matter instead of using rewards and punishments or threats;
+<br>• Does not use rankings, honors or medals to compare a student with another;</p>
+                    </div>    
+                    <div class="column1"  >
+                        <p>• Creates an atmosphere of care, fun and challenge that enables students to dare to try new things without the fear of being humiliated or punished;
+
+<br>• Involves the parents and family in the education of the children, by holding parenting seminars and family day programs;
+<br>• Enables students to be aware of the larger picture such as the environment, meaning of life, comparative religion, world peace and similar themes.</p>
+                    </div>   
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <div class="column Elementary"  >
+                        <h2>Elementary</h2>
+                        <img src="assets/img/elementary.JPG" style=" width:400px; height:200">  
+                    </div>
+                    <div class="column">
+                        <h2>Secondary</h2>
+                        <img src="assets/img/secondary.JPG" style=" width:400px; height:200">
+                    </div>
+                    <div class="column">
+                        <h2>Tertiary</h2>
+                        <img src="assets/img/tertiary.JPG" style=" width:400px; height:200">
+                    </div>
+                </div>
+                <br>
+                <div class="mission" id="mission">
+                    <h1 style="text-align:center;">Mission and Vision</h1>
+                    <div class="column1 "  >
+                        <p>• To educate and bring up children and young adults to become competent, well balanced, emotionally mature, socially responsible, morally upright and spiritually sensitive individuals.
+                        <br>
+                        <br>
+• To develop and promote an enlightened educational system that teaches the art and science of wise living in addition to achieving academic excellence- a system that can be shared with other institutions for the benefit of individuals and society</p>
+                         
+                    </div>
+                    <div class="column1 "  >
+                        <p>• To be a center of peace and harmony where people of different cultures will leave and learn together to help promote world peace and brotherhood without distinction of religion, race, sex or nationality, in the light of the ageless wisdom of life.
+                        <br>
+                        <br>
+• To help provide quality and right education to the less privileged children</p>                      
+                    </div>
+                </div>
+              <br>
+            <div id="curse" class="curse">
+                <h1>Programs Offered</h1>
+                 <div class="column2 curse">
+                 <h2 class="logo"><img src="assets/img/bsit.png" style="width:50px" > BSIT</a></h2>
+                 </div>   
+                 <div class="column2 curse" >
+                    <h2 class="logo"><img src="assets/img/psych.png" style="width:40px"> BSPSYCH</a></h2>
+                 </div>   
+                 <div class="column2 curse" >
+                    <h2 class="logo"><img src="assets/img/bsba.png" style="width:40px" > BSBA</a></h2>
+                 </div>   
+   
+                 <div class="column2 " >
+                    <h2 ><img src="assets/img/bsais.jpg" style="width:60px" > BSAIS</a></h2>
+                 </div>   
+                 
+                 <div class="column2 " >
+                    <h2 ><img src="assets/img/bsed.png" style="width:50px" > BSED</a></h2>
+                 </div>   
+                 
+                 <div class="column2 " >
+                    <h2 ><img src="assets/img/beed.png" style="width:40px" > BEED</a></h2>
+                 </div>  
+            </div>   
+
+              <br>
+                <div id="blog" class="section-title">
                     <h2>Blogs</h2>
                     <h3>See Our Latest <span>Blogs</span></h3>
                 </div>
@@ -107,10 +184,10 @@
             <div class="container">
                 <div class="row justify-content-center">
                     <div class="col-lg-6">
-                        <h4>Join Our Newsletter</h4>
-                        <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
+                        <h4>Interested on being a Linker?</h4>
+                        <p>enter your email for more updates.</p>
                         <form action="" method="post">
-                            <input type="email" name="email"><input type="submit" value="Subscribe">
+                            <input type="email" name="email"><input type="submit" value="submit">
                         </form>
                     </div>
                 </div>
@@ -122,35 +199,31 @@
                 <div class="row">
 
                     <div class="col-lg-3 col-md-6 footer-contact">
-                        <h3>BizLand<span>.</span></h3>
+                        <h3>GLC<span>.</span></h3>
                         <p>
-                            A108 Adam Street <br>
-                            New York, NY 535022<br>
-                            United States <br><br>
-                            <strong>Phone:</strong> +1 5589 55488 55<br>
-                            <strong>Email:</strong> info@example.com<br>
+                            Waling Waling St., Along Dona Aurora St. <br>
+                            Camarin Caloocan City Brgy.177<br>
+                            Philippines <br><br>
+                            <strong>Phone:</strong> (+632) 8961-5836<br>
+                            <strong>Email:</strong> GLC@example.com<br>
                         </p>
                     </div>
 
                     <div class="col-lg-3 col-md-6 footer-links">
                         <h4>Useful Links</h4>
                         <ul>
-                            <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
-                            <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
-                            <li><i class="bx bx-chevron-right"></i> <a href="#">Services</a></li>
-                            <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
-                            <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#home">Home</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#main">About us</a></li>
+                           
                         </ul>
                     </div>
 
                     <div class="col-lg-3 col-md-6 footer-links">
                         <h4>Our Services</h4>
                         <ul>
-                            <li><i class="bx bx-chevron-right"></i> <a href="#">Web Design</a></li>
-                            <li><i class="bx bx-chevron-right"></i> <a href="#">Web Development</a></li>
-                            <li><i class="bx bx-chevron-right"></i> <a href="#">Product Management</a></li>
-                            <li><i class="bx bx-chevron-right"></i> <a href="#">Marketing</a></li>
-                            <li><i class="bx bx-chevron-right"></i> <a href="#">Graphic Design</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#curse">Programs offered</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#mission">Mission and Vision</a></li>
+                            
                         </ul>
                     </div>
 
@@ -172,14 +245,10 @@
 
         <div class="container py-4">
             <div class="copyright">
-                &copy; Copyright <strong><span>BizLand</span></strong>. All Rights Reserved
+                &copy; Copyright <strong><span>Golden Link College</span></strong>. All Rights Reserved
             </div>
             <div class="credits">
-                <!-- All the links in the footer should remain intact. -->
-                <!-- You can delete the links only if you purchased the pro version. -->
-                <!-- Licensing information: https://bootstrapmade.com/license/ -->
-                <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/bizland-bootstrap-business-template/ -->
-                Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+               
             </div>
         </div>
     </footer><!-- End Footer -->
